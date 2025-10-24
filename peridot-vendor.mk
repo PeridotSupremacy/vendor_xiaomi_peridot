ab647212f886542861225eabe0b27926d9e3822f
Drenzzz <realdrenzzz@gmail.com>
2025-10-24 00:20:51 +0000

    peridot: Update from OS2.0.206.0.VNPMIXM

--- peridot-vendor.mk
+++ peridot-vendor.mk
@@ -1587,7 +1587,7 @@ PRODUCT_PACKAGES += \
     libskewknob \
     libslimclient \
     libsmemlog \
-    libsn100u_fw \
+    libsn220u_fw \
     libsnapdragoncolor-manager \
     libsnapdragoncolor-qdcm \
     libsns_device_mode_stub \
